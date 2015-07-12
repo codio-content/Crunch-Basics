02f97d1a76f2e198f85384b8cae520436b3edf4a
Elliott Mestas <elliott.mestas@gmail.com>
2015-07-12 22:24:09 +0200

    required changes to work with new browserify solution

--- .guides/tests/guide-script.js
+++ .guides/tests/guide-script.js
@@ -1,40 +1,40 @@
 function t_io(data) {
-  TESTS.SimpleOutputTest(data, [{min: 0, max: 100}], function(inp, vars) {
+  tests.SimpleOutputTest(data, [{min: 0, max: 100}], function(inp, vars) {
     return [inp[0]];
   });
 }
 
 function t_add100(data) {
-  TESTS.SimpleOutputTest(data, [{min: 0, max: 100}], function(inp, vars) {
+  tests.SimpleOutputTest(data, [{min: 0, max: 100}], function(inp, vars) {
     return [inp[0] + 100];
   });
 }
 
 function t_sub(data) {
-  TESTS.SimpleOutputTest(data, [{min: 0, max: 100}, {name: 'myNumber', min: 0, max: 100}], function(inp, vars) {
+  tests.SimpleOutputTest(data, [{min: 0, max: 100}, {name: 'myNumber', min: 0, max: 100}], function(inp, vars) {
     var var0 = vars[0].value;
     return [inp[0] - var0];
   });
 }
 
 function t_xTimesY(data) {
-  TESTS.SimpleOutputTest(data, [{min: 0, max: 100}, {min: 0, max: 100}], function(inp, vars) {
+  tests.SimpleOutputTest(data, [{min: 0, max: 100}, {min: 0, max: 100}], function(inp, vars) {
     return [inp[0] * inp[1]];
   });
 }
 
 function t_abc(data) {
-  TESTS.SimpleOutputTest(data, [{min: 0, max: 100}, {min: 0, max: 100}, {min: 0, max: 100}], function(inp, vars) {
+  tests.SimpleOutputTest(data, [{min: 0, max: 100}, {min: 0, max: 100}, {min: 0, max: 100}], function(inp, vars) {
     return [inp[0] * (inp[1] + inp[2])];
   });
 }
 
 function waitForCrunchScript() {
-  if (!window.CRUNCH_COMMON_LOADED) {
+  if (!window.TESTS_COMMON_LOADED) {
     setTimeout(waitForCrunchScript, 100);
     return;
   }
-  TESTS.SetupButtonTest();
+  tests.SetupButtonTest();
 }
 
 waitForCrunchScript();
